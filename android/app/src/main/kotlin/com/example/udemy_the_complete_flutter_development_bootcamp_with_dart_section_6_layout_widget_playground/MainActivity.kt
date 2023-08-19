c43c74dc0e013deff6b6530b776a49419bd2c6d7
danilodequeiroz <danilo.aq@gmail.com>
2023-08-19 11:01:38 -0300

    first commit, project created;

---
+++ android/app/src/main/kotlin/com/example/udemy_the_complete_flutter_development_bootcamp_with_dart_section_6_layout_widget_playground/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.udemy_the_complete_flutter_development_bootcamp_with_dart_section_6_layout_widget_playground
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
