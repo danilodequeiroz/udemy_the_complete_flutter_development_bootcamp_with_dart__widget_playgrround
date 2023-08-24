1ca04229956e0c230040ab986e88d4611b8e2ed9
danilodequeiroz <danilo.aq@gmail.com>
2023-08-24 01:06:41 -0300

    includes samples on  column_playground.dart

--- lib/column_playground.dart
+++ lib/column_playground.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 String textContent = "Column\nPlayground";
+double measurement = 128;
 
 class ColumnLayoutPlaygroundApp extends StatelessWidget {
   const ColumnLayoutPlaygroundApp({super.key});
@@ -11,90 +12,24 @@ class ColumnLayoutPlaygroundApp extends StatelessWidget {
         home: Scaffold(
             backgroundColor: Colors.red[100],
             body: SafeArea(
-              child: Column(
-                children: [symmetric(), all()],
-              ),
+              child: Column(children: [
+                for (MapEntry entry
+                    in [
+                      Colors.blue[500],
+                      Colors.red[500],
+                      Colors.white
+                    ].asMap().entries)
+                   container(index: entry.key, color: entry.value),
+              ]),
             )));
   }
 }
 
-symmetric() {
+Container container({int? index, Color? color}) {
   return Container(
-    height: 128,
-    width: 128,
-    margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 60),
-    color: Colors.orange[600],
-    child: Text(textContent),
-  );
-}
-
-all() {
-  return Container(
-    height: 128,
-    width: 128,
-    margin: const EdgeInsets.all(10),
-    color: Colors.orange[600],
-    child: Text(textContent),
-  );
-}
-
-fromLTRB() {
-  return Container(
-    height: 128,
-    width: 128,
-    margin: const EdgeInsets.fromLTRB(80, 20, 120, 60),
-    color: Colors.orange[600],
-    child: Text(textContent),
-  );
-}
-
-onlyLeft40() {
-  return Container(
-    height: 128,
-    width: 128,
-    margin: const EdgeInsets.only(left: 40),
-    color: Colors.orange[600],
-    child: Text(textContent),
-  );
-}
-
-onlyRight40() {
-  return Container(
-    height: 128,
-    width: 128,
-    margin: const EdgeInsets.only(right: 40),
-    color: Colors.orange[600],
-    child: Text(textContent),
-  );
-}
-
-onlyTop40() {
-  return Container(
-    height: 128,
-    width: 128,
-    margin: const EdgeInsets.only(top: 40),
-    color: Colors.orange[600],
-    child: Text(textContent),
-  );
-}
-
-onlyBottom40() {
-  return Container(
-    height: 128,
-    width: 128,
-    margin: const EdgeInsets.only(bottom: 40),
-    color: Colors.orange[600],
-    child: Text(textContent),
-  );
-}
-
-fromLTRBWithInnerPadding() {
-  return Container(
-    height: 128,
-    width: 128,
-    margin: const EdgeInsets.fromLTRB(80, 20, 120, 60),
-    padding: const EdgeInsets.all(20),
-    color: Colors.orange[600],
-    child: Text(textContent),
+    height: measurement,
+    width: measurement,
+    color: color,
+    child: Text("$textContent${index == 0 ? "" : " $index"}"),
   );
 }
